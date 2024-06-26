47bb4a39b992bf9492ffe79746ed643b12452600
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-06-26 17:30:40 +0900

    c: first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <section>introduction section</section>
+      <section>content part section</section>
+    </div>
+  );
+}
