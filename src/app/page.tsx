ed2467ca55d3a15ab30a3802e4b0fece2dfee439
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-06-26 17:56:13 +0900

    c: tailwindcss-question-mark 라이브러리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  const router = useRouter();
+
+  const handleRoute = () => {
+    router.push("/about");
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-center text-4xl font-bold">
+        Welcome to the Next.js + Tailwind CSS Starter
+      </h1>
+      <section>
+        {/* tab */}
+        <div className="flex justify-center">
+          <div className="flex space-x-4">
+            <a href="/about">About</a>
+            <a href="/articles">Articles</a>
+            <a href="/projects">Projects</a>
+          </div>
+        </div>
+      </section>
+      <section></section>
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "tailwindcss"
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -75,10 +75,10 @@ const config = {
       },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
-      }
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), require("tailwindcss-question-mark")],
+} satisfies Config;
 
-export default config
+export default config;
