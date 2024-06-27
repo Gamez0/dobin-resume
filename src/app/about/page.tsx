752fd936dd6271042eaef516a0676bba926ea75c
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-06-27 19:02:20 +0900

    c: Button theme test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function AboutPage() {
   return (
     <div>
       <h1>About Page</h1>
+      <section>
+        <Button>hello</Button>
+      </section>
       <section>introduction section</section>
       <section>content part section</section>
     </div>
