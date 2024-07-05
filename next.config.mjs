8f1bf8ae5d5e4f99ed4b0548c4aff404e18307c9
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-07-05 15:14:49 +0900

    u: script, next config for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/dobin-resume",
+  assetPrefix: "/dobin-resume/",
+};
 
 export default nextConfig;
