c556420f316695212f52a1158ba9b9fe1b16c9ad
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-07-05 18:13:52 +0900

    c: fix-me

--- src/app/common/Header/index.tsx
+++ src/app/common/Header/index.tsx
@@ -9,6 +9,7 @@ export default function Header() {
     <header className="flex items-center justify-between px-8 py-4">
       <div className="flex items-center gap-8">
         <h1 className="text-2xl font-bold">
+          {/* FIX-ME: use next/link because baseUrl changes in github pages */}
           <a href="/">DS</a>
         </h1>
         <Navigation />
