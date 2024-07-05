0e2390511835ee28fee84ee1ca4c8a7fc23c102c
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-07-05 11:59:56 +0900

    r: move careerData json to assets

--- src/app/about/CareerPreviewSection/index.tsx
+++ src/app/about/CareerPreviewSection/index.tsx
@@ -2,14 +2,13 @@
 
 import CareerCard from "./CareerCard";
 import type { CareerCardProps } from "./CareerCard/types";
-import careerData from "./careerData.json";
+import careerData from "@assets/careerData.json";
 
 const CAREER_DATA = careerData as CareerCardProps[];
 
 export default function CareerPreviewSection() {
   return (
     <section className="w-full bg-secondary px-4 py-16">
-      {/* <h2>career preview section</h2> */}
       <div className="inline-grid w-full grid-flow-row grid-cols-2  gap-8 px-[80px] lg:grid-cols-1 lg:px-0">
         {CAREER_DATA.map((career, index) => (
           <CareerCard key={index} {...career} />
