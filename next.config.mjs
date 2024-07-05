3b988f26edd25c5aef247221a9c5000102e35394
Dobin Shin <34051876+Gamez0@users.noreply.github.com>
2024-07-05 16:22:56 +0900

    Fix route without base path, delete baseUrl (#16)
    * c: add route rewrites
    
    * u: trailingSlash activate
    
    * u: async rewrites
    
    * u: basePath
    
    * d: basePath

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: "/",
-  assetPrefix: "/",
+  // basePath: "/",
+  // assetPrefix: "/",
   trailingSlash: true,
 
   async rewrites() {
