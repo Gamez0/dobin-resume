9d8533a6f2dfce7cee083f552813e6244cacf2c5
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-07-05 16:59:08 +0900

    u: fix by Navigation component

--- next.config.mjs
+++ next.config.mjs
@@ -9,23 +9,23 @@ const nextConfig = {
   basePath: isProd ? "/dobin-resume" : "",
   assetPrefix: isProd ? "/dobin-resume/" : "",
   // trailingSlash: true,
-  async rewrites() {
-    return [
-      // {
-      //   source: "/about",
-      //   destination: isProd ? "/dobin-resume/about" : "/about",
-      // },
-      // {
-      //   source: "/contact",
-      //   destination: isProd ? "/dobin-resume/contact" : "/contact",
-      // },
-      {
-        source: "/:path*",
-        destination: isProd ? "/dobin-resume/:path*" : "/:path*",
-      },
-      // Add more routes as needed
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     // {
+  //     //   source: "/about",
+  //     //   destination: isProd ? "/dobin-resume/about" : "/about",
+  //     // },
+  //     // {
+  //     //   source: "/contact",
+  //     //   destination: isProd ? "/dobin-resume/contact" : "/contact",
+  //     // },
+  //     // {
+  //     //   source: "/:path*",
+  //     //   destination: isProd ? "/dobin-resume/:path*" : "/:path*",
+  //     // },
+  //     // Add more routes as needed
+  //   ];
+  // },
 };
 
 export default nextConfig;
