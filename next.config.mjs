fbdbd15aa181756b75bce12ba8e07fbb69fbe531
do bin shin <34051876+Gamez0@users.noreply.github.com>
2024-07-05 16:42:30 +0900

    u: rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,29 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  // basePath: "/",
-  // assetPrefix: "/",
-  trailingSlash: true,
-
+  basePath: isProd ? "/dobin-resume" : "",
+  assetPrefix: isProd ? "/dobin-resume/" : "",
+  // trailingSlash: true,
   async rewrites() {
     return [
+      // {
+      //   source: "/about",
+      //   destination: isProd ? "/dobin-resume/about" : "/about",
+      // },
+      // {
+      //   source: "/contact",
+      //   destination: isProd ? "/dobin-resume/contact" : "/contact",
+      // },
       {
         source: "/:path*",
-        destination: "/dobin-resume/:path*",
+        destination: isProd ? "/dobin-resume/:path*" : "/:path*",
       },
+      // Add more routes as needed
     ];
   },
 };
