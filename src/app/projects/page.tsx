e0b916731eeb2956b4d40306537d0fd4ef965f7e
신도빈의 맥 <dobinshin@gmail.com>
2025-03-27 22:00:25 +0900

    c: fetch projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,37 @@
 "use client";
 
 import ProjectExperience from "@/model/project-experience";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const [projects, setProjects] = useState<ProjectExperience[]>([]);
 
-  // TODO: fetch projects from API
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const response = await fetch("/api/projects"); // 절대 경로로 수정
+      const data = await response.json();
+      setProjects(data);
+    };
+    console.log("herer");
+
+    fetchUserData();
+  }, []);
+
   return (
     <div>
       <h1>Page of Projects</h1>
+      {projects.length > 0
+        ? projects.map((project) => (
+            <div key={project.name}>
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
+              <p>
+                {project.startDate} - {project.endDate}
+              </p>
+              <p>Technologies: {project.technologies.join(", ")}</p>
+            </div>
+          ))
+        : "Loading..."}
       {/* Add your project components or content here */}
     </div>
   );
